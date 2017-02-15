7dd0782ab37951b7751f7a45a795b64ce8755e8d
mgerb42 <mgerb42@gmail.com>
2017-02-15 06:16:52 +0000

    need to fix cross compilation - CGO is the issue

--- webpack.prod.config.js
+++
@@ -1,6 +0,0 @@
-var webpack = require("./webpack.config.js")
-
-webpack.plugins[0].filename = "/static/[name].[hash].css";
-webpack.output.filename = '/static/[name].[hash].js';
-
-module.exports = webpack;
